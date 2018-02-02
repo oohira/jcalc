b6e01403c059b46eb8277bc19f3bc84c756eb114
oohira <oohira.dev@gmail.com>
2018-02-03 00:03:27 +0900

    字句解析器のダミー実装

---
+++ src/main/java/com/github/oohira/jcalc/token/Tokenizer.java
@@ -0,0 +1,43 @@
+package com.github.oohira.jcalc.token;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+/**
+ * 字句解析器.
+ *
+ * ソースコードを解析してトークン列を生成する.
+ */
+public class Tokenizer implements Iterator<Token>, Iterable<Token> {
+
+    private final Token[] tokens;
+
+    private int current = 0;
+
+    /**
+     * TODO: 字句解析機を実装するまでテスト用で使うコンストラクタ
+     *
+     * @param tokens トークン列
+     */
+    public Tokenizer(final Token... tokens) {
+        this.tokens = tokens;
+    }
+
+    @Override
+    public Iterator<Token> iterator() {
+        return this;
+    }
+
+    @Override
+    public boolean hasNext() {
+        return this.current < this.tokens.length;
+    }
+
+    @Override
+    public Token next() {
+        if (!hasNext()) {
+            throw new NoSuchElementException();
+        }
+        return this.tokens[this.current++];
+    }
+}
---
+++ src/test/java/com/github/oohira/jcalc/token/TokenizerTest.java
@@ -0,0 +1,46 @@
+package com.github.oohira.jcalc.token;
+
+import org.junit.Test;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+import static com.github.oohira.jcalc.token.TokenType.NUMBER;
+import static com.github.oohira.jcalc.token.TokenType.STRING;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+/**
+ * {@link Tokenizer} のテストクラス.
+ */
+public class TokenizerTest {
+
+    @Test
+    public void iterator() {
+        Tokenizer tokenizer = new Tokenizer(
+                new Token(NUMBER, "1"), new Token(STRING, "2"));
+
+        Token token;
+        Iterator<Token> it = tokenizer.iterator();
+
+        assertThat(it.hasNext(), is(true));
+        token = it.next();
+        assertThat(token.getType(), is(NUMBER));
+        assertThat(token.getText(), is("1"));
+
+        assertThat(it.hasNext(), is(true));
+        token = it.next();
+        assertThat(token.getType(), is(STRING));
+        assertThat(token.getText(), is("2"));
+
+        assertThat(it.hasNext(), is(false));
+        try {
+            it.next();
+            fail("should throw a NoSuchElementException");
+        } catch (NoSuchElementException e) {
+            assertTrue(true);
+        }
+    }
+}
